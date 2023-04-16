af0920c46ba93533364eece5754e8ceb09afa35e
vinay-h-a <vk18jan@gmail.com>
2023-04-16 17:40:56 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <section>
+          <h1>Redux Fundamentals Example</h1>
+        </section>
+      </nav>
+      <main>
+        <section className="medium-container">
+          <h2>Todos</h2>
+          <div className="todoapp">
+            <Counter />
+            {/* <Footer /> */}
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/features/counter/counter.slice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface CounterState {
+    value: number
+}
+
+const initialState: CounterState = {
+    value: 0,
+}
+
+export const counterSlice = createSlice({
+    name: 'COUNTER',
+    initialState,
+    reducers: {
+        increment: (state) => { state.value++ },
+        decrement: (state) => { state.value-- },
+        incrementByAmount: (state, action: PayloadAction<number>) => {
+            state.value += action.payload
+        }
+    }
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export default counterSlice.reducer
